dc5120026f9181018305dadcd6b3613f35df8d4c
amitkumar2089 <amitkumar2089@gmail.com>
2024-03-22 20:46:15 +0530

    added endpoints and modified lanchsetting

--- src/ToDoApi/Program.cs
+++ src/ToDoApi/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("ToDoDb"));
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoDb"));
 
 var app = builder.Build();
 
@@ -15,6 +16,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("api/todo", async (AppDbContext dbContext) =>
+{
+    var items = await dbContext.ToDoItems.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext dbContext, ToDoItem toDoItem) =>
+{
+    await dbContext.ToDoItems.AddAsync(toDoItem);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDoItem.Id}", toDoItem);
+});
 
 app.UseHttpsRedirection();
 
